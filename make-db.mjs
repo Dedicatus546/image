d8aff08b5b87641ad68bf421c4b8fefc2d86a2e1
Par9uet <1607611087@qq.com>
2024-02-21 11:03:01 +0800

    更新脚本

---
+++ make-db.mjs
@@ -0,0 +1,44 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const createDBFile = async () => {
+  await fs.writeFile(
+    "db.json",
+    JSON.stringify({
+      list: [],
+      total: 0,
+    })
+  );
+};
+
+const db = {
+  list: [],
+  total: 0,
+};
+
+const insertDB = async (dir) => {
+  const pathList = await fs.readdir(dir);
+  for (const p of pathList) {
+    const stat = await fs.stat(path.join(dir, p));
+    if (stat.isDirectory() && /\d+/g.test(p)) {
+      const nextDir = path.join(dir, p);
+      await insertDB(nextDir);
+    } else if (/^\d{15}\..*$/g.test(p)) {
+      db.list.push({
+        path: path.join(dir, p),
+        name: p,
+      });
+      db.total++;
+      console.log(db);
+    }
+  }
+};
+
+const updateDB = async () => {
+  await fs.writeFile("db.json", JSON.stringify(db));
+};
+
+await createDBFile();
+await insertDB(".");
+console.log(db);
+await updateDB();
